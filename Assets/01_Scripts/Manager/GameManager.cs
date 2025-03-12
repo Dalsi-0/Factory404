600ba409c9509c30cdaac0f6e11a30532870a56e
jj930220 <jj930220@gmail.com>
2025-03-12 17:16:54 +0900

    [Feat] 싱글톤 추가
    게임매니저에 싱글톤 상속

--- Assets/01_Scripts/Manager/GameManager.cs
+++ Assets/01_Scripts/Manager/GameManager.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
