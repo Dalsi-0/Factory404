0ed87b12316ab68c99add1485dccda94988e34e3
DasaemKim <kds000203@gmail.com>
2025-03-12 21:13:46 +0900

    (feat) 옵션 입출구 완성

--- Assets/01_Scripts/Manager/OptionManager.cs
+++ Assets/01_Scripts/Manager/OptionManager.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class OptionManager : MonoBehaviour
 {
-    public GameObject OptionPanel;
+    public GameObject OptionPanel; 
 
-    public void ToggleOptionPanel()
+    public void ToggleOptionPanel() // 옵션 패널 on/off 기능
     {
         if (OptionPanel != null)
         {
             //Debug.Log("1");
             OptionPanel.SetActive(!OptionPanel.activeSelf);
+        }
+    }
+
+    public void CloseOpionPanel()
+    {
+        if (OptionPanel != null)
+        {
+            OptionPanel.SetActive(false);
         }
     }
 }
