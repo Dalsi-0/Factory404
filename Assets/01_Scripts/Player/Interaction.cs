d67a019e59b0bc8b49b368dd665724909a400a46
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-03-13 19:57:30 +0900

    [Chore] 주석 추가

--- Assets/01_Scripts/Player/Interaction.cs
+++ Assets/01_Scripts/Player/Interaction.cs
@@ -22,6 +22,15 @@ public class Interaction : MonoBehaviour
 
     void Update()
     {
+        RayInteraction();
+    }
+    
+    /// <summary>
+    /// 레이캐스트를 통해 상호작용 가능한 오브젝트와 상호작용할 준비를 하는 함수
+    /// 상호작용 가능한 오브젝트가 레이캐스트에 닿으면 Outline이 생기고 이름과 상호작용 가능한 키를 UI에 띄운다 
+    /// </summary>
+    private void RayInteraction()
+    {
         Ray ray = mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
         Debug.DrawRay(ray.origin, ray.direction * maxCheckDistance, Color.red);
         RaycastHit hit;
@@ -59,18 +68,28 @@ public class Interaction : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 상호작용 가능한 오브젝트의 이름을 UI에 띄우는 함수
+    /// </summary>
     private void SetNameText()
     {
         nameText.gameObject.SetActive(true);
         nameText.text = curInteractable.GetNameText();
     }
 
+    /// <summary>
+    /// 상호작용 가능한 키를 UI에 띄우는 함수
+    /// </summary>
     private void SetInteractionText()
     {
         interactionText.gameObject.SetActive(true);
         interactionText.text = curInteractable.GetInteractionText();
     }
 
+    /// <summary>
+    /// Input System의 값을 받아 상호작용 가능한 오브젝트와 상호작용하는 함수
+    /// </summary>
+    /// <param name="context"> -> 상호작용 할 키 </param>
     public void OnInteractInput(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started && curInteractable != null)
