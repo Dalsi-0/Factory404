4033f64c93ad24c2c77e4734f3152e9362fa71cb
chajungto <50168549+chajungto@users.noreply.github.com>
2025-03-16 23:34:36 +0900

    [Feat] 스테이지 2 핵심 기믹 구현
    스테이지 2 핵심 기믹 구현

---
+++ Assets/01_Scripts/Item/Shelf.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shelf : InteractableObject, IInteractable
+{
+    [Header("Shelf 위에 있는 상자들 목록")]
+    public List<GameObject> crates;
+
+    public int activeCount;
+
+    void Awake()
+    {
+        foreach (var crate in crates)
+        {
+            crate.SetActive(false);
+        }
+    }
+
+    public string GetInteractionText()
+    {
+        string str = "'E'키를 상자 회수하기";
+
+        return str;
+    }
+
+    public string GetNameText()
+    {
+        return "선반";
+    }
+
+    public void OnInteract()
+    {
+        InterectWithShelf();
+    }
+
+    /// <summary>
+    /// 선반과 상호작용했을 때 상자가 있을 경우 상자를 줄이는 기능(최소개수제한)
+    /// </summary>
+    public void InterectWithShelf()
+    {
+        if (crates.Count >= 0 && activeCount > 0)
+        {
+            crates[activeCount - 1].SetActive(false);
+            activeCount--;
+        }
+    }
+}
