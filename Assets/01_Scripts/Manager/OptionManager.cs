f006a368e979d9cf0d04aef788d2ff4cbff0002e
dalsi <tkdwns8632@gmail.com>
2025-03-17 12:18:12 +0900

    [Feat] 옵션 슬라이더값을 수치로 표현

--- Assets/01_Scripts/Manager/OptionManager.cs
+++ Assets/01_Scripts/Manager/OptionManager.cs
@@ -4,28 +4,24 @@ using TMPro;
 using UnityEditor.Experimental.GraphView;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class OptionManager : MonoBehaviour
 {
-    public AudioSource audioSource;
     public SceneLoader sefse;
     public GameObject OptionPanel;
     public Slider bgmSlider; // Bgm 슬라이더
     public Slider sfxSlider; // SFX 슬라이더
     public Slider mouseSensitivitySlider; // 마우스 감도
     public GameObject continueButton; // 계속하기 버튼
-    public TextMeshProUGUI volumeText; // 볼륨 값
-    public TextMeshProUGUI sfxText; // 효과음 값
-    public TextMeshProUGUI sensitivityText; // 마우스 감도 값
+    public TextMeshProUGUI bgmSliderValueText;
+    public TextMeshProUGUI sfxSliderValueText;
+    public TextMeshProUGUI mouseSensitivitySliderValueText;
 
     private float mouseSensitivity = 1.0f; // 마우스 감도 기본값
     private float xRotation = 0f;
     private int currentStage; // 현재 스테이지
-    private string sensitivityKey = "MouseSensitivity";
-    private int MouseSensitivity;
 
     public void ToggleOptionPanel() // 옵션 패널 on/off 기능
     {
@@ -34,7 +30,7 @@ public class OptionManager : MonoBehaviour
             bool wasActive = OptionPanel.activeSelf;
             OptionPanel.SetActive(!wasActive);
 
-            if (wasActive) 
+            if (wasActive)
             {
                 Savevalue();
             }
@@ -52,20 +48,20 @@ public class OptionManager : MonoBehaviour
     void Start()
     {
         // 슬라이더 값 변경
-        bgmSlider.onValueChanged.AddListener(delegate { SetBgmVolume((int)bgmSlider.value); });
-        sfxSlider.onValueChanged.AddListener(delegate { SetSfxVolume((int)sfxSlider.value); });
-        mouseSensitivitySlider.onValueChanged.AddListener(delegate { UpdateSmouseSensitivitySlider((int)mouseSensitivitySlider.value); }); ;
+        bgmSlider.onValueChanged.AddListener(SetBgmVolume);
+        sfxSlider.onValueChanged.AddListener(SetSfxVolume);
 
         // 처음으로 시작할때 볼륨
         bgmSlider.value = PlayerPrefs.GetFloat("BgmVolume", 1f);
         sfxSlider.value = PlayerPrefs.GetFloat("SfxVolume", 1f);
-        
 
 
         ApplySound(); // 볼륨적용
 
-        mouseSensitivity = PlayerPrefs.GetInt(sensitivityKey, 1);
+        mouseSensitivity = PlayerPrefs.GetFloat("MouseSensitivity", 1.0f);
         mouseSensitivitySlider.value = mouseSensitivity; // 저장된 감도 불러오기
+
+        mouseSensitivitySlider.onValueChanged.AddListener(UpdateSmouseSensitivitySlider); // 감도 조절
 
         //Cursor.lockState = CursorLockMode.Locked; // 마우스 커서 고정
 
@@ -76,48 +72,26 @@ public class OptionManager : MonoBehaviour
         ContinueButton();
     }
 
-    public void SetVolume(int volume)
-    {
-        volumeText.text = volume.ToString();
-        audioSource.volume = volume / 100f; // 0-100 -> 0-1 변환
-        PlayerPrefs.SetInt("BgmVolume", volume);
-    }
-
-    public void SetSfx(int volume)
-    {
-        sfxText.text = volume.ToString();
-        PlayerPrefs.SetInt("SfxVolume", volume);
-    }
-
-    public void SetSensitivity(int sensitivity)
-    {
-        sensitivityText.text = sensitivity.ToString();
-        PlayerPrefs.SetInt(sensitivityKey, sensitivity);
-    }
-
     public void SetBgmVolume(float volume) // BgmVolume 조절
     {
         bgmSlider.value = volume;
+        bgmSliderValueText.text = (bgmSlider.value * 100f).ToString();
     }
 
     public void SetSfxVolume(float volume) //SfxVolume 조절
     {
         sfxSlider.value = volume;
+        sfxSliderValueText.text = (sfxSlider.value * 100f).ToString();
     }
 
     public void ApplySound()
     {
-        int bgm = PlayerPrefs.GetInt("BgmVolume", 100);
-        int sfx = PlayerPrefs.GetInt("SfxVolume", 100);
-        int sensitivity = PlayerPrefs.GetInt(sensitivityKey, 5);
-
-        audioSource.volume = bgm / 100f;
+        //OptionManager.Instance.
     }
-
-
     private void UpdateSmouseSensitivitySlider(float value)
     {
         mouseSensitivity = value;
+        mouseSensitivitySliderValueText.text = (mouseSensitivitySlider.value * 100f).ToString();
     }
     public float GetSensitivity()
     {
