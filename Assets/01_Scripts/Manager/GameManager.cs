efa7c03e3bfb3306520af19f8b95aee715ecaf68
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-03-18 20:29:13 +0900

    [Fix] 자잘한 버그 수정
    1. GameManager 안 쓰는 using문 제거
    2. 4번 일지에서 공장장 생일이 나오는 현상 수정

--- Assets/01_Scripts/Manager/GameManager.cs
+++ Assets/01_Scripts/Manager/GameManager.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.U2D;
 
